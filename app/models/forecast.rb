090d657a3f7b0114bea7908c2bd237b583f50497
AnnaErshova <anna.ershova@gmail.com>
2015-07-27 19:37:11 -0400

    all done

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -55,9 +55,10 @@ attr_accessor :zip_output, :weather_output, :url, :city_slug, :temperature, :hou
     self.collect_hours
     self.collect_temperature
     self.collect_humidity
+    self.collect_heat_index
   end
 
-  def calculate_heat_index
+  def collect_heat_index
 
     i = 0
     temp_array = self.temperature
@@ -66,29 +67,29 @@ attr_accessor :zip_output, :weather_output, :url, :city_slug, :temperature, :hou
 
     while i < self.temperature.length do
 
-      temp = temp_array[i]
-      rh = humidity_array[i]
+      temp = temp_array[i].to_f
+      rh = humidity_array[i].to_f
 
-      simple_heat_index = (0.5 * (temp + 61 + ((temp-68)*1.2) + (rh*0.094))).to_i
+      simple_heat_index = (0.5 * (temp + 61 + ((temp-68)*1.2) + (rh*0.094.to_f))).to_i
       full_heat_index = (-42.379 + (2.04901523*temp) + (10.14333127*rh) - (0.22475541*temp*rh) - (0.00683783*temp*temp) - (0.05481717*rh*rh) + (0.00122874*temp*temp*rh) + (0.00085282*temp*rh*rh) - (0.00000199*temp*temp*rh*rh)).to_i
       
-      if  (simple_heat_index+temp)/2 >= 80
+      if  (simple_heat_index+temp)/2 >= 80 
         # if low humidity and hot, use full_heat_index and appropriate adjustment
         if ((rh < 13) && (temp >= 80) && (temp < 112))
           heat_index_array << (full_heat_index-(((13-rh)/4)*((17-((temp-95).abs))/17)**0.5).to_i)
-          # if high humidity and hot
+          # if high humidity and hot 
         elsif ((rh > 85) && (temp >= 80) && (temp <= 87))
           heat_index_array << (full_heat_index-(((rh-85)/10) * ((87-temp)/5)).to_i)
           # all other scenarios -- use a full heat index
         else 
           heat_index_array << full_heat_index
         end
-        # use a simple heat index
-      else
+      else # use a simple heat index
         heat_index_array << simple_heat_index
       end # ends outer nested loop
         i += 1
     end # ends while
+    # binding.pry
     self.heat_index = heat_index_array
   end # ends calculate_heat_index
 
