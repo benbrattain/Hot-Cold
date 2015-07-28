c8b5e1c8e082771b44ff87debc9912d0826cc345
AnnaErshova <anna.ershova@gmail.com>
2015-07-27 20:17:02 -0400

    refactors heat index formula with variables, but not class variables yet

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -61,36 +61,30 @@ attr_accessor :zip_output, :weather_output, :url, :city_slug, :temperature, :hou
   def collect_heat_index
 
     i = 0
-    temp_array = self.temperature
-    humidity_array = self.humidity
-    heat_index_array = []
+    self.heat_index = []
 
     while i < self.temperature.length do
+      temp = self.temperature[i].to_f
+      humidity = self.humidity[i].to_f
+      simple_heat_index = (0.5 * (temp + 61 + ((temp-68)*1.2) + (humidity*0.094))).to_i
+      full_heat_index = (-42.379 + (2.04901523*temp) + (10.14333127*humidity) - (0.22475541*temp*humidity) - (0.00683783*temp*temp) - (0.05481717*humidity*humidity) + (0.00122874*temp*temp*humidity) + (0.00085282*temp*humidity*humidity) - (0.00000199*temp*temp*humidity*humidity)).to_i
+      dry_and_hot_adjustment = (((13-humidity)/4)*(((17-((temp-95).abs))/17)**0.5))  
+      humid_and_hot_adjustment = ((humidity-85)/10) * ((87-temp)/5)
 
-      temp = temp_array[i].to_f
-      rh = humidity_array[i].to_f
-
-      simple_heat_index = (0.5 * (temp + 61 + ((temp-68)*1.2) + (rh*0.094))).to_i
-      full_heat_index = (-42.379 + (2.04901523*temp) + (10.14333127*rh) - (0.22475541*temp*rh) - (0.00683783*temp*temp) - (0.05481717*rh*rh) + (0.00122874*temp*temp*rh) + (0.00085282*temp*rh*rh) - (0.00000199*temp*temp*rh*rh)).to_i
-      
       if  (simple_heat_index+temp)/2 >= 80
-        # if low humidity and hot, use full_heat_index and appropriate adjustment
-        if ((rh < 13) && (temp >= 80) && (temp < 112))
-          heat_index_array << (full_heat_index-(((13-rh)/4)*((17-((temp-95).abs))/17)**0.5).to_i)
-          # if high humidity and hot
-        elsif ((rh > 85) && (temp >= 80) && (temp <= 87))
-          heat_index_array << (full_heat_index-(((rh-85)/10) * ((87-temp)/5)).to_i)
-          # all other scenarios -- use a full heat index
+        if ((humidity < 13) && (temp >= 80) && (temp < 112)) # especially dry and hot
+          self.heat_index << (full_heat_index-dry_and_hot_adjustment).to_i
+        elsif ((humidity > 85) && (temp >= 80) && (temp <= 87)) # especially humid and hot
+          self.heat_index << (full_heat_index-humid_and_hot_adjustment).to_i
         else 
-          heat_index_array << full_heat_index
+          self.heat_index << full_heat_index
         end
-        # use a simple heat index
       else
-        heat_index_array << simple_heat_index
+        self.heat_index << simple_heat_index
       end # ends outer nested loop
         i += 1
     end # ends while
-    self.heat_index = heat_index_array
+    self.heat_index
   end # ends calculate_heat_index
 
 end # ends class
