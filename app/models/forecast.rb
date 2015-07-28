b5d4c0787e7de8cdfa8b110b4477435c3758d377
AnnaErshova <anna.ershova@gmail.com>
2015-07-27 21:00:52 -0400

    adds intermediary variable to wind chill formula

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -107,7 +107,8 @@ attr_accessor :zip_output,
     while i < self.wind_speed.length do
       temp = self.temperature[i].to_f
       wind_speed = self.wind_speed[i].to_f
-      self.wind_chill << (35.74+(0.6215*temp)-(35.75*(wind_speed**0.16))+(0.4275*temp*(wind_speed**0.16))).to_i
+      wind_chill_calc = (35.74+(0.6215*temp)-(35.75*(wind_speed**0.16))+(0.4275*temp*(wind_speed**0.16))).to_i
+      self.wind_chill << wind_chill_calc
       i += 1
     end # ends while
     self.wind_chill
