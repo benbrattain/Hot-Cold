30689ecc95f3dbea50f46f30a9690e3f5d1a1b87
AnnaErshova <anna.ershova@gmail.com>
2015-07-28 16:55:48 -0400

    rearranges methods in heat index calculator in a more logical order

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -91,17 +91,19 @@ attr_accessor :zip_output,
 
   # http://www.srh.noaa.gov/images/epz/wxcalc/windChill.pdf
   def collect_wind_chill
-    i = 0
-    self.wind_chill = []
-    while i < self.wind_speed.length do
-      temp = self.temperature[i].to_f
-      wind_speed = self.wind_speed[i].to_f
-      wind_chill_calc = (35.74+(0.6215*temp)-(35.75*(wind_speed**0.16))+(0.4275*temp*(wind_speed**0.16))).to_i
-      self.wind_chill << wind_chill_calc
-      i += 1
-    end # ends while
-    self.wind_chill
-  end # ends collect_wind_chill
+    # i = 0
+    # self.wind_chill = []
+    # while i < self.wind_speed.length do
+    #   temp = self.temperature[i].to_f
+    #   wind_speed = self.wind_speed[i].to_f
+    #   wind_chill_calc = (35.74+(0.6215*temp)-(35.75*(wind_speed**0.16))+(0.4275*temp*(wind_speed**0.16))).to_i
+    #   self.wind_chill << wind_chill_calc
+    #   i += 1
+    # end 
+    # self.wind_chill
+    wind_chill_calculator = WindChillCalculator.new(self)
+    self.wind_chill = wind_chill_calculator.calculate
+  end 
 
   def set_now_statement
     temperature_now = self.temperature[0].to_i # in F
--- app/models/wind_chill_calculator.rb
+++ app/models/wind_chill_calculator.rb
@@ -10,7 +10,7 @@ class WindChillCalculator
   def calculate
     i = 0
     self.wind_chill = []
-    while i < self.wind_speed.length do
+    while i < self.wind_speed.length do # has no wind_speed
       temp = forecast.temperature[i].to_f
       wind_speed = forecast.wind_speed[i].to_f
       wind_chill_calc = (35.74+(0.6215*temp)-(35.75*(wind_speed**0.16))+(0.4275*temp*(wind_speed**0.16))).to_i
