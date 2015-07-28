7ad64e52e7d0eed1a32db3edda04fc8cefd27aeb
AnnaErshova <anna.ershova@gmail.com>
2015-07-28 16:59:41 -0400

    adds a functionining wind-chill-calculator-class

--- app/models/wind_chill_calculator.rb
+++ app/models/wind_chill_calculator.rb
@@ -10,14 +10,25 @@ class WindChillCalculator
   def calculate
     i = 0
     self.wind_chill = []
-    while i < self.wind_speed.length do # has no wind_speed
-      temp = forecast.temperature[i].to_f
-      wind_speed = forecast.wind_speed[i].to_f
+    while i < 36 do 
+    # while i < self.wind_speed.length do 
+      temp = temp(i)
+      wind_speed = wind_speed(i)
+      # humidity = humidity(i)
+      # wind_speed = forecast.wind_speed[i].to_f
       wind_chill_calc = (35.74+(0.6215*temp)-(35.75*(wind_speed**0.16))+(0.4275*temp*(wind_speed**0.16))).to_i
       self.wind_chill << wind_chill_calc
       i += 1
     end # ends while
     self.wind_chill
+  end
+
+  def temp(i)
+    temp = forecast.temperature[i].to_f
+  end
+
+  def wind_speed(i)
+    wind_speed = forecast.wind_speed[i].to_f
   end
 
 end
