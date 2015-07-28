9d85278becc8ccc26226e23771ad426751ec211e
AnnaErshova <anna.ershova@gmail.com>
2015-07-28 16:49:21 -0400

    starts refactoring with wind chill calculator class

--- app/models/wind_chill_calculator.rb
+++ app/models/wind_chill_calculator.rb
@@ -1,3 +1,23 @@
 class WindChillCalculator
 
+  attr_accessor :forecast, 
+                :wind_chill
+
+  def initialize(forecast)
+    @forecast = forecast
+  end
+
+  def calculate
+    i = 0
+    self.wind_chill = []
+    while i < self.wind_speed.length do
+      temp = forecast.temperature[i].to_f
+      wind_speed = forecast.wind_speed[i].to_f
+      wind_chill_calc = (35.74+(0.6215*temp)-(35.75*(wind_speed**0.16))+(0.4275*temp*(wind_speed**0.16))).to_i
+      self.wind_chill << wind_chill_calc
+      i += 1
+    end # ends while
+    self.wind_chill
+  end
+
 end
